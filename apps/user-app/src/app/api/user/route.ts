35cf3bf9b0adcfbc94fb3c647a076172e73dc3c7
Shishir435 <shishirchaurasiya435@gmail.com>
2024-03-26 06:01:30 +0530

    Fix formatting and add missing semicolon in user route

--- apps/user-app/src/app/api/user/route.ts
+++ apps/user-app/src/app/api/user/route.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 import { PrismaClient } from "@repo/db/client";
 
 const client = new PrismaClient();
 
-export const GET = async ():Promise<NextResponse> => {
-    await client.user.create({
-        data: {
-            age: "asd",
-            name: "adsads"
-        }
-    })
-    return NextResponse.json({
-        message: "hi there"
-    })
-}
+export const GET = async (): Promise<NextResponse> => {
+  await client.user.create({
+    data: {
+      age: "asd",
+      name: "adsads",
+    },
+  });
+  return NextResponse.json({
+    message: "hi there",
+  });
+};
